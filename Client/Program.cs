da25e6ea21f8c311529a8765f09f7a7ab352ce46
Curufir <curufir@gmail.com>
2023-02-05 12:35:24 +0000

    Add Serilog

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,13 @@
 ﻿using Autofac;
 using Client;
 using Client.Services;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("./Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 ContainerBuilder builder = new();
 
